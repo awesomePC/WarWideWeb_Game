820f410787eab8e3d4b76ff71e8c3cb27bec10fb
machinePC <billycrow268@gmail.com>
2022-10-19 08:33:47 +0900

    UI_design

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import { useAuth } from "./contexts/AuthContext";
 import Header from "./components/Header";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 import Router from "../src/router/index";
 
 export default function App() {
@@ -8,7 +8,7 @@ export default function App() {
   return (
     <BrowserRouter>
       <Header />
-      <Router />
+      {isLoggedIn ? <Router /> : <LoggedOutText/>}
     </BrowserRouter>
   );
 }
