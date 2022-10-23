09211a796969b063deaa47df4d5e7fee8b0c6a25
machinePC <billycrow268@gmail.com>
2022-10-23 17:51:55 -0400

    peripheral

--- client/src/pages/game.js
+++ client/src/pages/game.js
@@ -1,11 +1,11 @@
 import React, { useState, useEffect } from 'react'
 import { useNavigate } from 'react-router-dom';
 import Dashboard from '../components/dashboard/dashboard';
-import TransactionTable from '../components/dashboard/transaction';
 import Profile from '../components/game/profile';
 import { useAuth } from '../contexts/AuthContext';
 import History from '../components/tables/hitory';
-
+import Security from '../components/dashboard/security';
+import Account from '../components/dashboard/account';
 import '../styles/gamepage.css'
 
 const GamePage = () => {
@@ -18,8 +18,8 @@ const GamePage = () => {
             case 1: setString(<History filter='deposit' />); break;
             case 2: setString(<History filter='withdraw' />); break;
             case 3: setString(<History fileter='all' />); break;
-            case 4: setString(<Dashboard />); break;
-            case 5: setString(<Dashboard />); break;
+            case 4: setString(<Account />); break;
+            case 5: setString(<Security />); break;
             default: setString(<Dashboard />)
         }
     }, [id]);
