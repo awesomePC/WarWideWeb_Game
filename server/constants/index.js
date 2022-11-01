77e3cf3d2d7e7559f646312e9b1d0437b613655c
machinePC <billycrow268@gmail.com>
2022-11-01 16:37:21 -0400

    final

--- server/constants/index.js
+++ server/constants/index.js
@@ -1,6 +1,6 @@
 const ORIGIN = '*'
 const BASECLIENTURL = "http://localhost"
-const PORT = process.env.PORT || 80
+const PORT = process.env.PORT || 443
 const ETHERPRICE = 1500;
 const PRICE1 = 0.5
 const PRICE2 = 1
